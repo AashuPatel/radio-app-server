20f2db7f3eee3c4f86d3ff437737bf4207ca2ccf
AashuPatel <aashutoshpatelprofessional@gmail.com>
2024-12-09 01:14:51 +0530

    Your commit message

--- App.js
+++ App.js
@@ -86,7 +86,7 @@ app.post('/delete', async (req, res)=>{
 
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
